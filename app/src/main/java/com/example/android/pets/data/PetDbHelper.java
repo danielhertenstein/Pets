a8aa247746811ea53f448cf2aed3f7e200052a14
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2017-11-27 12:05:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -0,0 +1,34 @@
+package com.example.android.pets.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.android.pets.data.PetContract.PetEntry;
+
+public class PetDbHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "shelter.db";
+
+    public PetDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String SQL_CREATE_ENTRIES = "CREATE TABLE " + PetEntry.TABLE_NAME + " ("
+                + PetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + PetEntry.COLUMN_PET_NAME + "TEXT NOT NULL, "
+                + PetEntry.COLUMN_PET_BREED + "TEXT, "
+                + PetEntry.COLUMN_PET_GENDER + "INT NOT NULL, "
+                + PetEntry.COLUMN_PET_WEIGHT + "INT NOT NULL DEFAULT 0);";
+        sqLiteDatabase.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + PetEntry.TABLE_NAME + ";";
+        sqLiteDatabase.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(sqLiteDatabase);
+    }
+}
